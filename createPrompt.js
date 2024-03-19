1566d98f076e489138d5355edc1c2b3320cb8f90
Plk <planck@schrodingernft.ai>
2024-03-19 22:59:10 +0800

    Remove only cat

--- createPrompt.js
+++ createPrompt.js
@@ -7,7 +7,6 @@ function createPrompt(config, trait_args) {
     var traitsFormatted = trait_args.map(formatTrait);
     prompt = config.prefix + traitsFormatted.join('\n') + '\n'
     prompt = prompt + ' The image should contain the full-body shot of the main character.'
-    prompt = prompt + ' The image should contain one and only one cat.'
     prompt = prompt + ' The generated image should not contain any text or labels.'
     return prompt;
 }
